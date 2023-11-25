071545bd506bb289ad110d6a11ca0d84d3ec5965
yarramneninikhil <124859874+yarramneninikhil@users.noreply.github.com>
2023-11-25 23:01:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const http = require("http");
 const fs = require("fs");
 
+let fileContent = "";
+
 const server = http.createServer((req, res) => {
   const url = req.url;
-  const method = req.method; // Retrieve the HTTP method
-
-  // Maintain an array to store user messages
-  const messages = [];
+  const method = req.method;
 
   if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>ReadAndWrite</title></head>");
-    res.write("<body>");
-
-    // Display existing messages at the top of the form
-    for (const message of messages) {
-      res.write(`<p>${message}</p>`);
+    if (fs.existsSync("./hello.txt")) {
+      fileContent = fs.readFileSync("./hello.txt", "utf8");
     }
 
-    res.write('<form action="/message" method="POST">');
-    res.write('<input type="text" name="message">');
-    res.write('<button type="submit">SEND</button>');
-    res.write("</form>");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<body>");
+    res.write(`<p>${fileContent}</p>`);
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>'
+    );
     res.write("</body>");
     res.write("</html>");
     return res.end();
@@ -34,33 +31,16 @@ const server = http.createServer((req, res) => {
     });
 
     req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split("=")[1];
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
 
-      // Add the new message to the messages array
-      messages.push(message);
-
-      res.write("<html>");
-      res.write("<head><title>ReadAndWrite</title></head>");
-      res.write("<body>");
-
-      // Display all messages, including the new one
-      for (const msg of messages) {
-        res.write(`<p>${msg}</p>`);
-      }
-
-      res.write('<form action="/message" method="POST">');
-      res.write('<input type="text" name="message">');
-      res.write('<button type="submit">SEND</button>');
-      res.write("</form>");
-      res.write("</body>");
-      res.write("</html>");
-      res.end();
+      fs.writeFileSync("./hello.txt", message);
+      fileContent = message;
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-    return;
   }
-
-  // Handle other routes or request methods here
 });
 
-server.listen(3000);
+server.listen(3001);
